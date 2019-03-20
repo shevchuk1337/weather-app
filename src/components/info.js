ce4b0d9fc3ca103b514dd7df9aa286046c16c6a5
Anton Shevchuk <anton.shevchuk1208@email.ua>
2019-03-20 15:56:28 +0200

    Test332

---
+++ src/components/info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Info = (props) => {
+    return(
+        <div>
+        <h2>Погодное приложение</h2>
+        <p>Узнайте погоду в Вашем городе!</p>
+        </div>
+    );
+}
+
+export default Info
